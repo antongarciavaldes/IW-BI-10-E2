5ee55f394915fe83dbee5279381e90c3a5fbab0b
jonayo7 <113512121+jonayo7@users.noreply.github.com>
2023-12-04 09:03:13 +0100

    urls.py
    a

--- appCarOutlet/urls.py
+++ appCarOutlet/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index_marcas, name='index'),
     path('marcas/<int:marca_id>/', views.show_marca, name='detail'),
-    path('marcas/<int:marca_id>/coches', views.index_coches, name='coches'),
+    path('marcas/<int:marca_id>/coches/', views.index_coches, name='coches'),
     path('categorias/<int:categoria_id>', views.show_categoria, name='categoria'),
     path('coches/<int:coche_id>', views.show_coche, name='coche'),
 ]
