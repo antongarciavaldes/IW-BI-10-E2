484b17dddc9f38c637023653defef913c1c27553
antongarciavaldes <114146531+antongarciavaldes@users.noreply.github.com>
2023-12-04 13:44:17 +0100

    ?????

--- appCarOutlet/urls.py
+++ appCarOutlet/urls.py
@@ -7,11 +7,7 @@ urlpatterns = [
     path('marcas/<int:marca_id>/coches/', views.index_coches, name='coches'),
     path('categorias/<int:categoria_id>', views.show_categoria, name='categoria'),
     path('coches/<int:coche_id>', views.show_coche, name='coche'),
-<<<<<<< Updated upstream
     path('formulario_marca/',views.marca_form_view, name = 'marca_form'),
     path('formulario_categoria/',views.categoria_form_view, name = 'categoria_form'),
     path('formulario_coche/',views.coche_form_view, name = 'coche_form')
-=======
-    path('set-language/', views.set_language, name='set_language'),
->>>>>>> Stashed changes
 ]
